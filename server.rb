12099ae39522c1327b5eb950c19926f2b4505671
SamRain229 <joshuakmeadows@gmail.com>
2018-07-10 22:29:41 -0400

    small two page static site

---
+++ server.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+get '/' do
+    File.read(File.join('public', 'index.html'))
+end
+
+get "/samrain" do
+    File.read(File.join('public' , 'samrain.html'))
+  end
+
+get "images/Headshot.jpg" do
+  File.set("images", "Headshot.jpg")
+end
