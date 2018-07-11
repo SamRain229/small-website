a94a9f430343c785b5aaee0f569fd5145f8573f6
SamRain229 <joshuakmeadows@gmail.com>
2018-07-11 17:19:20 -0400

    Finished

--- server.rb
+++ server.rb
@@ -7,6 +7,6 @@ get "/samrain" do
     File.read(File.join('public', 'samrain.html'))
   end
 
-get "images/Headshot.jpg" do
-  File.set(File.join("images", "Headshot.jpg"))
-end
+# get "/images/Headshot.jpg" do
+#   File.set(File.join("images", "Headshot.jpg"))
+# end
